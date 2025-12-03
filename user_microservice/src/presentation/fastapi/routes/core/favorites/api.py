6a8379a9aa6e6f2e8615eff77b4ef2918b15689b
LizaG000 <elizaveta.vladimirovna.g@gmail.com>
2025-12-03 15:42:01 +0300

    правлена юзерсхема и исправлен возвращение схемы

--- user_microservice/src/presentation/fastapi/routes/core/favorites/api.py
+++ user_microservice/src/presentation/fastapi/routes/core/favorites/api.py
@@ -1,15 +1,23 @@
 from dishka.integrations.fastapi import DishkaRoute
 from dishka.integrations.fastapi import FromDishka
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from fastapi import status
 from src.usecase.favorites.create import CreateFavoritesUsecase
+from src.usecase.favorites.get import GetFavoritesUsecase
 from src.application.schemas.favorites import FavoriteSchema
+from src.application.schemas.common import PaginationSchema
 from src.usecase.favorites.schemas import GetCreateFavoritesSchema
 
 ROUTER = APIRouter(route_class=DishkaRoute, tags=["Favorites"])
 
 @ROUTER.post('', status_code=status.HTTP_200_OK)
-async def create_users(
+async def create_favorites(
     usecase: FromDishka[CreateFavoritesUsecase],
     data: GetCreateFavoritesSchema) -> FavoriteSchema:
     return await usecase(data=data)
+
+@ROUTER.get('', status_code=status.HTTP_200_OK)
+async def get_favorites(
+    usecase: FromDishka[GetFavoritesUsecase],
+    data: PaginationSchema = Query()) -> None:
+    await usecase(data=data)
